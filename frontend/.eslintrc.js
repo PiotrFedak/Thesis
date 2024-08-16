31dcfa01251c86c4a7a3a46481600e21d6c30ba7
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-08-16 15:41:58 +0200

    eslint changes

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,26 +1,19 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      jest: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-    ],
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint'],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-    },
-  };
+  },
+  rules: {},
+};
