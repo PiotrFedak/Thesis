abf114077dcbc0608f7b6e7ce6789389e2680a63
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-10-10 20:58:54 +0200

    New Map image

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import NBAVerse from '../Images/NBAVerse.png';
 import HeroMap from './HeroMap';
 import TeamHero from './TeamHero';
+import NBAVerse from '../Images/NBAVerse.png';
 
 const Hero = () => {
   return (
--- frontend/src/components/HeroMap.jsx
+++ frontend/src/components/HeroMap.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import NBAVerse from '../Images/NBAVerse.png';
 import { Link } from 'react-router-dom';
+import Test from '../Images/Test.png';
 
 const HeroMap = () => {
   return (
-    <div name="home" className="w-full h-screen flex flex-col justify-between">
-      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
-        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse w-full max-w-[1240px] m-auto">
+        <div className="flex justify-center w-full lg:w-1/2 lg:justify-end">
+          <img
+            src={Test}
+            alt="Map of NBA Clubs"
+            className="w-full h-auto md:w-[120%] md:scale-100"
+          />
+        </div>
+
+        <div className="text-center lg:text-left lg:w-1/2 flex flex-col justify-center px-6 py-8">
           <p className="text-2xl">Map</p>
           <h1 className="py-3 text-5xl md:text-7xl font-bold">
             NBAVerse Map Page
           </h1>
-          <p className="text-2xl mr-2 mt-2">
+          <p className="text-2xl mt-2 mb-2">
             Interactive map displaying the locations of NBA clubs for easy
             navigation.
           </p>
-          <div className="md:py-2 md:px-0 py-4 px-6 sm:w-[60%] my-6">
+          <div className="py-4">
             <Link
               to="/Map"
-              className="px-10 py-4 text-xl font-semibold bg-custom-blue text-white border-4 border-custom-blue rounded-md hover:bg-white hover:text-custom-blue transition-colors"
+              className="px-8 py-2 text-xl font-semibold bg-custom-blue text-white border-4 border-custom-blue rounded-md hover:bg-white hover:text-custom-blue transition-colors"
             >
               Go to Map
             </Link>
           </div>
-        </div>
-        <div>
-          <img className="w-full" src={NBAVerse} alt="/" />
         </div>
       </div>
     </div>
