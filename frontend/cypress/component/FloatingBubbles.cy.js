120bdb1a9392a8a057a177e2778107c2e7191af7
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-10-22 01:36:32 +0200

    Better Navbar

--- frontend/cypress/component/FloatingBubbles.cy.js
+++ frontend/cypress/component/FloatingBubbles.cy.js
@@ -1,9 +1,9 @@
-import FloatingBubbles from "../../src/layouts/FloatingBubbles";
+import FloatingBubbles from '../../src/layouts/FloatingBubbles';
 
 describe('FloatingBubbles Component Test', () => {
-  it('should render floating bubbles check lenght and size of the SVG icons', () => {
+  it('should render floating bubbles animation, check lenght and size of the SVG icons', () => {
     cy.mount(<FloatingBubbles />);
-    
+
     cy.get('svg').should('have.length', 8);
     cy.get('svg').each(($el) => {
       cy.wrap($el)
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import {
   IoCloseCircleOutline,
   IoMenu,
   IoBasketballOutline,
 } from 'react-icons/io5';
-import Switcher from './common/Switcher';
-import { useTranslation } from 'react-i18next';
 import Flag from 'react-flagkit';
+import Switcher from './common/Switcher';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -27,6 +27,11 @@ const Navbar = () => {
       ? 'p-4 text-custom-blue border-b-2 border-custom-blue dark:border-custom-red dark:text-custom-red'
       : 'p-4';
   };
+
+  const showGetStartedButton =
+    location.pathname === '/' ||
+    location.pathname === '/Map' ||
+    location.pathname === '/Teams';
 
   return (
     <div className="fixed top-0 w-full bg-custom-white dark:bg-custom-black shadow-xl z-50">
@@ -74,6 +79,17 @@ const Navbar = () => {
         </div>
       </div>
 
+      {showGetStartedButton && (
+        <div className="hidden 2xl:block">
+          <Link
+            to="/Auth"
+            className="top-8 flex absolute right-12 px-2 py-2 text-xl font-semibold bg-custom-blue dark:bg-custom-red text-white border-2 rounded-md hover:bg-white dark:hover:bg-custom-black hover:text-custom-blue dark:hover:text-custom-white transition-colors"
+          >
+            {i18n.t('getStarted')}
+          </Link>
+        </div>
+      )}
+
       <div
         onClick={handleNavChange}
         className="block md:hidden z-50 absolute top-10 right-2"
@@ -102,6 +118,9 @@ const Navbar = () => {
         </li>
         <li className={getLinkClass('/Players')}>
           <Link to="/Players">{i18n.t('players')}</Link>
+        </li>
+        <li className={getLinkClass('/Auth')}>
+          <Link to="/Auth">{i18n.t('auth')}</Link>
         </li>
         <li className="absolute top-10 right-16">
           <Switcher />
