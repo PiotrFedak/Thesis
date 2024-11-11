8d559137f1bd97106955f2f63b82276cec056a5b
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-11-11 20:11:55 +0100

    Added Tests fir User Controller

---
+++ backend/tests/Unit/UserControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testGetCurrentUserWhenAuthenticated(): void
+    {
+        $user = User::factory()->create(['isAdmin' => false]);
+        $token = $user->createToken("TestToken")->plainTextToken;
+    
+        $response = $this->withHeaders(["Authorization" => "Bearer $token"])
+                         ->getJson("/api/getCurrentUser");
+    
+        $response->assertStatus(200)
+                 ->assertJson([
+                     "user" => [
+                         "id" => $user->id,
+                         "name" => $user->name,
+                         "email" => $user->email,
+                     ],
+                 ]);
+    }
+    
+
+    public function testGetCurrentUserWhenUnauthenticated(): void
+    {
+        $response = $this->getJson("/api/getCurrentUser");
+
+        $response->assertStatus(401)
+                 ->assertJson([
+                     "message" => "Unauthenticated.",
+                 ]);
+    }
+}
