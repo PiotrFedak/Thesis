73701ebbfecf9f618361024a900a6c940e6f2e9e
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-11-20 18:38:42 +0100

    TeamSeederTest added and CSF

--- backend/app/Models/Team.php
+++ backend/app/Models/Team.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,19 +10,20 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     use HasFactory;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'team_id',
-        'location',
-        'name',
-        'city',
-        'conference',
-        'division',
-        'address',
-        'icon_url',
+        "team_id",
+        "location",
+        "name",
+        "city",
+        "conference",
+        "division",
+        "address",
+        "icon_url",
     ];
 }
